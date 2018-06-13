511a0c14c9b59637dd4dd32ec22e16a79f59afc5
Andrey Falko <afalko@salesforce.com>
2018-06-12 18:41:56 -0700

    Upgrade to revised interface

--- src/main/java/net/afalko/zipkin/filter/ExampleSpanFilter.java
+++ src/main/java/net/afalko/zipkin/filter/ExampleSpanFilter.java
@@ -1,17 +1,23 @@
 package net.afalko.zipkin.filter;
 
-import zipkin.Span;
-import zipkin.filter.FilterActivatedException;
-import zipkin.filter.SpanFilter;
-import zipkin.storage.Callback;
+import zipkin2.Callback;
+import zipkin2.Span;
+import zipkin2.collector.CollectorMetrics;
+import zipkin2.collector.filter.SpanFilter;
 
+import javax.xml.ws.http.HTTPException;
 import java.util.Collections;
 import java.util.List;
 
-public class ExampleSpanFilter implements SpanFilter<Span> {
-    public List<Span> process(List<Span> spans, Callback<Void> callback) {
+public class ExampleSpanFilter implements SpanFilter {
+    public List block(Callback<Void> callback) {
         // Just blocks everything
-        callback.onError(new FilterActivatedException("All spans blocked by design", 429));
+        callback.onError(new HTTPException(429));
         return Collections.emptyList();
+    }
+
+    public List<Span> process(List<Span> list, CollectorMetrics metrics, Callback<Void> callback) {
+        metrics.incrementSpansDropped(list.size());
+        return block(callback);
     }
 }
--- src/main/java/net/afalko/zipkin/filter/ExampleSpanFilterAutoConfiguration.java
+++ src/main/java/net/afalko/zipkin/filter/ExampleSpanFilterAutoConfiguration.java
@@ -2,12 +2,12 @@ package net.afalko.zipkin.filter;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import zipkin.filter.SpanFilter;
+import zipkin2.collector.filter.SpanFilter;
 
 @Configuration
 public class ExampleSpanFilterAutoConfiguration {
     @Bean
-    public SpanFilter<?> exampleFilterSpan() {
+    public SpanFilter exampleFilterSpan() {
         return new ExampleSpanFilter();
     }
 }
